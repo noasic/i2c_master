21d57fcef9271351cfa8e1785cb8890899f5c786
Guy Eschemann <guy@noasic.com>
2016-06-03 17:22:27 +0200

    add header

--- vhdl/i2c_master_pkg.vhd
+++ vhdl/i2c_master_pkg.vhd
@@ -1,24 +1,61 @@
+-------------------------------------------------------------------------------
+--
+--  I2C Master (package)
+--
+--  Author(s):
+--    Guy Eschemann, Guy.Eschemann@gmail.com
+--
+-------------------------------------------------------------------------------
+--
+-- Copyright (c) 2016, Guy Eschemann
+-- All rights reserved.
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 
+-- 1. Redistributions of source code must retain the above copyright notice, this
+--    list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+-- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-- 
+-- The views and conclusions contained in the software and documentation are those
+-- of the authors and should not be interpreted as representing official policies,
+-- either expressed or implied, of the FreeBSD Project.
+--
+-------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package i2c_master_pkg is
+    type t_i2c_instr is (I2C_START, I2C_STOP, I2C_ADDR, I2C_WR_BYTE, I2C_RD_BYTE);
 
-  type t_i2c_instr is (I2C_START, I2C_STOP, I2C_ADDR, I2C_WR_BYTE, I2C_RD_BYTE);
+    type t_frequency is range 0 to 2147483647 units kHz;
+        MHz = 1000 kHz;
+        GHz = 1000 MHz;
+    end units;
 
-  type t_frequency is range 0 to 2147483647 units kHz;
-    MHz = 1000 kHz;
-    GHz = 1000 MHz;
-  end units;
-
-  function "/"(a : integer; b : t_frequency) return time;
+    function "/"(a : integer; b : t_frequency) return time;
 
 end package i2c_master_pkg;
 
 package body i2c_master_pkg is
-  function "/"(a : integer; b : t_frequency) return time is
-  begin
-    return a * 1 ms / (b / 1 kHz);
-  end function;
+    function "/"(a : integer; b : t_frequency) return time is
+    begin
+        return a * 1 ms / (b / 1 kHz);
+    end function;
 
 end package body i2c_master_pkg;
